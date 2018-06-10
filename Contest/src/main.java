6d48f6849c580d63c0d9d521978bc5c57d50db50
user <user@user-PC>
2018-06-10 17:16:14 +0900

    123

--- Contest/src/main.java
+++ Contest/src/main.java
@@ -3,6 +3,6 @@ public class main {
 	public static void main(String args[])throws Exception
 	{
 		
-		System.out.println("asdfas2433d332343fasdf");
+		System.out.println("asdfas2432223d332343fasdf");
 	}
 }
